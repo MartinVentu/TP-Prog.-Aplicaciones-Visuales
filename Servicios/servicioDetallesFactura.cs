681cce806e6ed4f0a3ea5607a58cdf3fb665749d
BraianLP <107091647+BraianLP@users.noreply.github.com>
2022-10-10 16:46:14 -0300

    agregado servicio factura y detalle

---
+++ Servicios/servicioDetallesFactura.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TPPAV.Entidades;
+using TPPAV.Repositorios;
+
+namespace TPPAV.Servicios
+{
+    public class servicioDetallesFactura
+    {
+        private repositorioDetalleFactura repositorio;
+
+        public servicioDetallesFactura()
+        {
+            repositorio = new repositorioDetalleFactura();
+        }
+
+        public bool RegistrarDetalle(DetallesFactura detalle)
+        {
+            if (repositorio.RegistrarDetalle(detalle) > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public List<DetallesFactura> ConsultarDetalle(Facturas factura)
+        {
+            var detalles = repositorio.ConsultarDetalles(factura);
+            return detalles;
+        }
+    }
+}
---
+++ Servicios/servicioFacturas.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TPPAV.Entidades;
+using TPPAV.Repositorios;
+
+namespace TPPAV.Servicios
+{
+    public class servicioFacturas
+    {
+        private repositorioFactura repositorio;
+        public servicioFacturas()
+        {
+            repositorio = new repositorioFactura();
+        }
+
+        public bool RegistrarFactura(Facturas factura)
+        {
+            if (repositorio.RegistrarFactura(factura) > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public List<Facturas> ConsultarFacturas()
+        {
+            var facturas = repositorio.ConsultarFacturas();
+            return facturas;
+        }
+
+        public List<Facturas> ConsultarFacturas(DateTime fecha)
+        {
+            var facturas = repositorio.ConsultarFacturas(fecha);
+            return facturas;
+        }
+        public List<Facturas> ConsultarFacturas(Clientes cliente)
+        {
+            var facturas = repositorio.ConsultarFacturas(cliente);
+            return facturas;
+        }
+        public List<Facturas> ConsultarFacturas(TiposFacturas tipo)
+        {
+            var facturas = repositorio.ConsultarFacturas(tipo);
+            return facturas;
+        }
+        public List<Facturas> ConsultarFacturas(Vendedores vendedor)
+        {
+            var facturas = repositorio.ConsultarFacturas(vendedor);
+            return facturas;
+        }
+
+    }
+}
